0dc0d109ac99d779b99ac0e70f74c26b85621575
_Chi_ <iwuagwuchibuzor@gmail.com>
2022-02-23 15:17:48 +0100

    tried resolving the blocked-mix-content error

--- src/services/form.js
+++ src/services/form.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = "http://segserver.herokuapp.com"
+const baseURL = "//segserver.herokuapp.com"
 
 export const postForm = async (payload) => {
   return await axios.post(`${baseURL}/form`, payload)
